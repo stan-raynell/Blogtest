8ddcf390f8e1a1993083191faf58c121dfa57c6b
stan-raynell <dunn2000@mail.russh-keygen -t rsa -b 4096 -C dunn2000@mail.rucat ~/.ssh/id_rsa.pubgit config --global user.email dunn2000@mail.ru>
2023-03-14 14:38:26 +0300

    Seed adj

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
-User.create(email: "dunn2000@mail.ru",
-            password: "Foobar",
-            admin: true)
+admin = User.create(email: "dunn2000@mail.ru",
+                    password: "Foobar",
+                    admin: true)
 
-30.times do |n|
+10.times do |n|
+  article = Article.create!(title: Faker::Book.title,
+                  body: Faker::Lorem.sentence(word_count: 30),
+                  user: admin, status: "public")
+  Comment.create!(body: Faker::Lorem.sentence(word_count: 10),
+                  article: article, status: "archived", user: admin)
+  Comment.create!(body: Faker::Lorem.sentence(word_count: 10),
+                  article: article, status: "private", user: admin)
+  Comment.create!(body: Faker::Lorem.sentence(word_count: 10),
+                  article: article, status: "public", user: admin)
+end
+
+40.times do |n|
   email = "test-#{n + 1}@s.com"
   password = "password"
   User.create!(email: email, password: password)
@@ -11,7 +23,7 @@ end
 30.times do |n|
   article = Article.create!(title: Faker::Book.title,
                             body: Faker::Lorem.sentence(word_count: 30),
-                            user: User.find(n + 1), status: "public")
+                            user: User.find(n + 2), status: "public")
   Comment.create!(body: Faker::Lorem.sentence(word_count: 10),
                   article: article, status: "archived", user: User.find(n + 1))
   Comment.create!(body: Faker::Lorem.sentence(word_count: 10),
