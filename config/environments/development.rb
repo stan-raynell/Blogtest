1bdd35567366e72d94bd1a33eb240ef66233f6a6
stan-raynell <dunn2000@mail.russh-keygen -t rsa -b 4096 -C dunn2000@mail.rucat ~/.ssh/id_rsa.pubgit config --global user.email dunn2000@mail.ru>
2023-03-17 22:54:41 +0300

    add host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "nodpoint.tplinkdns.com"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
